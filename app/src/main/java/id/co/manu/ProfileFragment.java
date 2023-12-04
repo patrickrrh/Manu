b66aea63f059a30f63c79a219bc516f2c38c88f6
lixkelik <lixkelik@gmail.com>
2023-12-04 22:38:34 +0700

    init

--- app/src/main/java/id/co/manu/ProfileFragment.java
+++ app/src/main/java/id/co/manu/ProfileFragment.java
@@ -8,14 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment {
-
-    public HomeFragment() {
-
+public class ProfileFragment extends Fragment {
+    public ProfileFragment() {
+        // Required empty public constructor
     }
 
-    public static HomeFragment newInstance() {
-        HomeFragment fragment = new HomeFragment();
+    public static ProfileFragment newInstance() {
+        ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
@@ -30,6 +29,6 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
