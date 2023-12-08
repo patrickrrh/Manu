9bc885735254c8f39ed9318bd0ba18aaef873918
patrickrrh <patrickrh31@gmail.com>
2023-12-09 01:03:47 +0700

    kelarrr (sisa foto profile)

--- app/src/main/java/id/co/manu/views/ProfileFragment.java
+++ app/src/main/java/id/co/manu/views/ProfileFragment.java
@@ -13,16 +13,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 import id.co.manu.MainActivity;
 import id.co.manu.R;
+import id.co.manu.model.Customer;
 import id.co.manu.viewmodel.AuthViewModel;
+import id.co.manu.viewmodel.CustomerViewModel;
 
 public class ProfileFragment extends Fragment {
 
     private AuthViewModel authViewModel;
+    private CustomerViewModel customerViewModel;
+    private TextView userId, name, email, phoneNumber;
     private Button signOutBtn;
 
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,7 +49,26 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        userId = view.findViewById(R.id.idUserTxt);
+        name = view.findViewById(R.id.namaUserTxt);
+        email = view.findViewById(R.id.emailUserTxt);
+        phoneNumber = view.findViewById(R.id.teleponUserTxt);
+
+        customerViewModel = new ViewModelProvider(this).get(CustomerViewModel.class);
+
+        customerViewModel.getCustomerData().observe(getViewLifecycleOwner(), new Observer<Customer>() {
+            @Override
+            public void onChanged(Customer customer) {
+                // Update UI with the new customer data
+                userId.setText(customer.getUid());
+                name.setText(customer.getName());
+                email.setText(customer.getEmail());
+                phoneNumber.setText(customer.getPhoneNumber());
+            }
+        });
+        return view;
     }
 
     @Override
